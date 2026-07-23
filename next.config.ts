f4bffafcc74a0855cf48acbcbf046483bbc9a6a8
Isaias Tenorio <tenorioretis@gmail.com>
2026-07-22 22:50:57 -0300

    Actualizar Next.js 16 y corregir configuracion

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-
+  turbopack: {},
 };
 
 export default nextConfig;
